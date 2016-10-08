32d64bb067c5c482aac5bd5ae6f8db6c7accac9a
Enzo Bianchi <bianch.enzo@gmail.com>
2016-10-08 16:25:56 +0200

    'coucou'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,91 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  public lastTemperatureS1: number;
+  public last10temperatureS1: Array<number>;
+
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
+  // lineChart
+  public lineChartData: Array<any>;
+
+  public lineChartLabels: Array<any> = ['#1', '#2', '#3', '#4', '#5', '#6', '#7','#8','#9','#10'];
+  public lineChartOptions: any = {
+    animation: false,
+    responsive: true
+  };
+  public lineChartColors: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+
+  temperatureS1: FirebaseListObservable<any[]>;
+  
+  constructor(af: AngularFire) {
+    this.last10temperatureS1 = new Array<number>();
+    this.temperatureS1 = af.database.list('temperatureS1', {
+      preserveSnapshot: true,
+      query: {
+        limitToLast: 10,
+        orderByKey: true
+      }
+    });
+
+    this.temperatureS1.subscribe(items =>{
+      this.last10temperatureS1.splice(0);
+
+      items.forEach(item =>{
+        this.last10temperatureS1.push(item.val())
+      });
+      
+    this.lineChartData = [
+      { data: this.last10temperatureS1, label: 'Sonde 1' },
+    ];
+
+      this.lastTemperatureS1 = this.last10temperatureS1[this.last10temperatureS1.length-1];
+    });
+    
+
+    this.lineChartData = [
+      { data: this.last10temperatureS1, label: 'Sonde 1' }
+      //{ data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      //{ data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
+    ];
+
+  
+
+
+  }
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAw1WAKY8yO0ye16IiUdOZ9sVt211XUYyM',
+  authDomain: 'prodotti-46c93.firebaseapp.com',
+  databaseURL: 'https://prodotti-46c93.firebaseio.com',
+  storageBucket: 'prodotti-46c93.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
